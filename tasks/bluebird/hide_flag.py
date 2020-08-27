6ab02918ef45785c4abf1a55ed4171e9d5aac148
Jakub Nowak <jakub.jakub.nowak@gmail.com>
2020-08-27 23:40:06 +0200

    [bluebird] Gzip Bluebird.bmp

--- tasks/bluebird/hide_flag.py
+++ tasks/bluebird/hide_flag.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import io
+from subprocess import check_call, CalledProcessError
 
+try:
+    check_call(['gzip', '-d', 'Bluebird.bmp.gz'])
+except CalledProcessError:
+    pass
 
 FLAG = b'CTF{Us3le55-Byt3Z}'
 with open('Bluebird.bmp', 'rb') as f:
@@ -11,3 +16,5 @@ for i, x in enumerate(FLAG):
     data[0x8d + 4*i] = x
 with open('Bluebird.bmp', 'wb') as f:
     f.write(data)
+
+check_call(['gzip', 'Bluebird.bmp'])
