694afb8d3b41e0a0695e4abea435c3e987abd67a
Jakub Nowak <jakub.jakub.nowak@gmail.com>
2020-08-29 03:08:03 +0200

    [interceptor] Write key on separate thread

--- tasks/interceptor/client.go
+++ tasks/interceptor/client.go
@@ -4,15 +4,19 @@ import "net"
 import "fmt"
 import "bufio"
 
-func main() {
-
-  // connect to server
-  conn, _ := net.Dial("tcp", "ctf.staszic.waw.pl:43958")
-  for {
-    conn.Write([]byte("74uekFFoluaAMyvANtwc3sKnnoloMV\n"))
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    if message != "" {
-      fmt.Print("Did not get any reply!\n")
-    }
+func handle (conn net.Conn, finished chan bool) {
+  conn.Write([]byte("74uekFFoluaAMyvANtwc3sKnnoloMV\n"))
+  message, _ := bufio.NewReader(conn).ReadString('\n')
+  if message == "" || message == "\n" {
+    fmt.Print("Did not get any reply!\n")
   }
+  finished <- true
+}
+
+func main () {
+  finished := make(chan bool)
+  conn, _ := net.Dial("tcp", "ctf.staszic.waw.pl:43958")
+  go handle(conn, finished)
+  <- finished
+  fmt.Print("Wziuuuuu!\n")
 }
